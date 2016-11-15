1bbbed1c4617d03d4ae57dd7c2c836bdee9dc381
Trung Tran <ahzhong11491@gmail.com>
2016-11-15 15:09:52 +0900

    Create recurrent_keras.py

---
+++ recurrent_keras.py
@@ -0,0 +1,65 @@
+from __future__ import print_function
+import matplotlib.pyplot as plt
+import numpy as np
+import time
+import csv
+from keras.models import Sequential
+from keras.layers.core import Dense, Activation, Dropout
+from keras.layers.recurrent import LSTM, SimpleRNN
+from keras.layers.wrappers import TimeDistributed
+import argparse
+from RNN_utils import *
+
+# Parsing arguments for Network definition
+ap = argparse.ArgumentParser()
+ap.add_argument('-data_dir', default='./data/test.txt')
+ap.add_argument('-batch_size', type=int, default=50)
+ap.add_argument('-layer_num', type=int, default=2)
+ap.add_argument('-seq_length', type=int, default=50)
+ap.add_argument('-hidden_dim', type=int, default=500)
+ap.add_argument('-generate_length', type=int, default=500)
+ap.add_argument('-nb_epoch', type=int, default=20)
+ap.add_argument('-weights', default='')
+args = vars(ap.parse_args())
+
+DATA_DIR = args['data_dir']
+BATCH_SIZE = args['batch_size']
+HIDDEN_DIM = args['hidden_dim']
+SEQ_LENGTH = args['seq_length']
+
+GENERATE_LENGTH = args['generate_length']
+LAYER_NUM = args['layer_num']
+
+# Creating training data
+X, y, VOCAB_SIZE, ix_to_char = load_data(DATA_DIR, SEQ_LENGTH)
+
+# Creating and compiling the Network
+model = Sequential()
+model.add(LSTM(HIDDEN_DIM, input_shape=(None, VOCAB_SIZE), return_sequences=True))
+for i in range(LAYER_NUM - 1):
+  model.add(LSTM(HIDDEN_DIM, return_sequences=True))
+model.add(TimeDistributed(Dense(VOCAB_SIZE)))
+model.add(Activation('softmax'))
+model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
+
+# Generate some sample before training to know how bad it is!
+generate_text(model, args['generate_length'], VOCAB_SIZE, ix_to_char)
+
+nb_epoch = 0
+
+# Training if there is no trained weights specified
+if args['weights'] == '':
+  while True:
+    print('\n\n')
+    model.fit(X, y, batch_size=BATCH_SIZE, verbose=1, nb_epoch=1)
+    nb_epoch += 1
+    generate_text(model, GENERATE_LENGTH)
+    if nb_epoch % 10 == 0:
+      model.save_weights('checkpoint_{}_epoch_{}.hdf5'.format(HIDDEN_DIM, nb_epoch))
+
+# Else, loading the trained weights and performing generation only
+else:
+  # Loading the trained weights
+  model.load_weights(args['weights'])
+  generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
+  print('\n\n')
