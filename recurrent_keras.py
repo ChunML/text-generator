75347bdd6c8a5cabb1dfe90697bfc29ac2814a55
Trung Tran <ahzhong11491@gmail.com>
2016-11-17 15:56:29 +0900

    Update recurrent_keras.py

--- recurrent_keras.py
+++ recurrent_keras.py
@@ -27,6 +27,7 @@ DATA_DIR = args['data_dir']
 BATCH_SIZE = args['batch_size']
 HIDDEN_DIM = args['hidden_dim']
 SEQ_LENGTH = args['seq_length']
+WEIGHTS = args['weights']
 
 GENERATE_LENGTH = args['generate_length']
 LAYER_NUM = args['layer_num']
@@ -45,25 +46,27 @@ model.compile(loss="categorical_crossentropy", optimizer="rmsprop")
 
 # Generate some sample before training to know how bad it is!
 generate_text(model, args['generate_length'], VOCAB_SIZE, ix_to_char)
-if not args['weights'] == '':
-  model.load_weights(args['weights'])
 
-nb_epoch = 0
+if not WEIGHTS == '':
+  model.load_weights(WEIGHTS)
+  nb_epoch = int(WEIGHTS[WEIGHTS.rfind('_') + 1:WEIGHTS.find('.')])
+else:
+  nb_epoch = 0
 
 # Training if there is no trained weights specified
-if args['mode'] == 'train' or args['weights'] == '':
+if args['mode'] == 'train' or WEIGHTS == '':
   while True:
-    print('\n\n')
+    print('\n\nEpoch: {}\n'.format(nb_epoch))
     model.fit(X, y, batch_size=BATCH_SIZE, verbose=1, nb_epoch=1)
     nb_epoch += 1
     generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
     if nb_epoch % 10 == 0:
-      model.save_weights('checkpoint_{}_epoch_{}.hdf5'.format(HIDDEN_DIM, nb_epoch))
+      model.save_weights('checkpoint_layer_{}_hidden_{}_epoch_{}.hdf5'.format(LAYER_NUM, HIDDEN_DIM, nb_epoch))
 
 # Else, loading the trained weights and performing generation only
-elif args['weights'] == '':
+elif WEIGHTS == '':
   # Loading the trained weights
-  model.load_weights(args['weights'])
+  model.load_weights(WEIGHTS)
   generate_text(model, GENERATE_LENGTH, VOCAB_SIZE, ix_to_char)
   print('\n\n')
 else:
